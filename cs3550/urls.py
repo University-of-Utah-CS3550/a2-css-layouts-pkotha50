6a6770a72da36253aebb1091ae375e9d5716e3e3
pkotha50 <142963339+pkotha50@users.noreply.github.com>
2025-02-23 13:46:10 -0700

    Phase 1 done, asmt 3

--- cs3550/urls.py
+++ cs3550/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from grades import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('profile/', views.profile),
+    path('profile/login/', views.login_form),
+    path('<int:assignment_id>/', views.assignment),
+    path('<int:assignment_id>/submissions/', views.submissions),
 ]
