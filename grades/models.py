338b176fc672d78aa17cdcec7cef43868732b95c
pkotha50 <142963339+pkotha50@users.noreply.github.com>
2025-02-23 15:32:07 -0700

    Phase 2 done, asmt 3

--- grades/models.py
+++ grades/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User, Group
 from django.db import models
 
 # Create your models here.
+class Assignment(models.Model):
+    title = models.CharField(max_length=199)
+    description = models.TextField(null=True, blank=True)
+    deadline = models.DateTimeField()
+    weight = models.IntegerField()
+    points = models.IntegerField(default=100)
+
+class Submission(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    grader = models.ForeignKey(User, related_name='graded_set', null=True, blank=True, on_delete=models.SET_NULL)
+    file = models.FileField()
+    score = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
