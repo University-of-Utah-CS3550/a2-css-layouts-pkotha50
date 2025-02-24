c96c5bef17f5ff9dec5300e9e8195d07c875791c
pkotha50 <142963339+pkotha50@users.noreply.github.com>
2025-02-23 22:29:23 -0700

    Phase 4 done, asmt 3

--- grades/views.py
+++ grades/views.py
@@ -1,5 +1,9 @@
 from . import models
+from django.contrib.auth.models import User, Group
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Assignment
+from .models import Submission
 
 # Create your views here.
 def index(request):
@@ -7,7 +11,21 @@ def index(request):
     return render(request, "index.html", {'assignments': assignments})
 
 def assignment(request, assignment_id):
-    return render(request, "assignment.html")
+    assignment = get_object_or_404(Assignment, id=assignment_id)
+
+    total_submissions = assignment.submission_set.count()
+    grader = User.objects.get(username='g')
+    user_submissions = grader.graded_set.filter(assignment=assignment).count()
+
+    total_students = models.Group.objects.get(name="Students").user_set.count()
+
+    pass_data = {
+        "assignment": assignment,
+        "total_submissions": total_submissions,
+        "user_submissions": user_submissions,
+        "total_students": total_students,
+    }
+    return render(request, "assignment.html", pass_data)
 
 def submissions(request, assignment_id):
     return render(request, "submissions.html")
